cb2293c4385b35ef7839c1db0bf9564ce60f7cb0
mikemccomb <mikemccomb@gmail.com>
2023-02-21 14:10:35 -0600

    created items

---
+++ store_item.rb
@@ -0,0 +1,11 @@
+# Ruby Syntax
+
+shoes = { :color => "red", :size => "large", :price => 39.99, :in_stock => false }
+hats = { :color => "blue", :size => "small", :price => 9.99, :in_stock => true }
+gloves = { :color => "black", :size => "medium", :price => 12.49, :in_stock => true }
+
+# Java Syntax
+
+shoes = { color: "red", size: "large", price: 39.99, in_stock: false }
+hats = { color: "blue", size: "small", price: 9.99, in_stock: true }
+gloves = { color: "black", size: "medium", price: 12.49, in_stock: true }
