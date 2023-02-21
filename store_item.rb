5573e9c8b4075779d58de8e0df3f923bf33d8875
mikemccomb <mikemccomb@gmail.com>
2023-02-21 14:18:37 -0600

    test statements

--- store_item.rb
+++ store_item.rb
@@ -9,3 +9,9 @@ gloves = { :color => "black", :size => "medium", :price => 12.49, :in_stock => t
 shoes = { color: "red", size: "large", price: 39.99, in_stock: false }
 hats = { color: "blue", size: "small", price: 9.99, in_stock: true }
 gloves = { color: "black", size: "medium", price: 12.49, in_stock: true }
+
+puts "#{shoes[:size]} #{shoes[:color]} shoes cost #{shoes[:price]}"
+
+puts "#{hats[:size]} #{hats[:color]} shoes cost #{hats[:price]}"
+
+puts "#{gloves[:size]} #{gloves[:color]} shoes cost #{gloves[:price]}"
