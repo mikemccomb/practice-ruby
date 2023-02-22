a4ce65c210e2f20ed89d8b1a7924dabc6bc0dd01
mikemccomb <mikemccomb@gmail.com>
2023-02-22 11:48:37 -0600

    clean up commenting

--- store_item.rb
+++ store_item.rb
@@ -1,20 +1,22 @@
 # Ruby Syntax
 
-#shoes = { :color => "red", :size => "large", :price => 39.99, :in_stock => false }
-#hats = { :color => "blue", :size => "small", :price => 9.99, :in_stock => true }
-#gloves = { :color => "black", :size => "medium", :price => 12.49, :in_stock => true }
+# shoes = { :color => "red", :size => "large", :price => 39.99, :in_stock => false }
+# hats = { :color => "blue", :size => "small", :price => 9.99, :in_stock => true }
+# gloves = { :color => "black", :size => "medium", :price => 12.49, :in_stock => true }
 
 # Java Syntax
 
-#shoes = { color: "red", size: "large", price: 39.99, in_stock: false }
-#hats = { color: "blue", size: "small", price: 9.99, in_stock: true }
-#gloves = { color: "black", size: "medium", price: 12.49, in_stock: true }
+# shoes = { color: "red", size: "large", price: 39.99, in_stock: false }
+# hats = { color: "blue", size: "small", price: 9.99, in_stock: true }
+# gloves = { color: "black", size: "medium", price: 12.49, in_stock: true }
 
-#puts "The #{shoes[:size]} #{shoes[:color]} shoes cost $#{shoes[:price]}."
+# puts "The #{shoes[:size]} #{shoes[:color]} shoes cost $#{shoes[:price]}."
 
-#puts "The #{hats[:size]} #{hats[:color]} shoes cost $#{hats[:price]}."
+# puts "The #{hats[:size]} #{hats[:color]} shoes cost $#{hats[:price]}."
 
-#puts "The #{gloves[:size]} #{gloves[:color]} shoes cost $#{gloves[:price]}."
+# puts "The #{gloves[:size]} #{gloves[:color]} shoes cost $#{gloves[:price]}."
+
+# Classy Syntax
 
 class Store
   attr_reader :color, :size, :price, :in_stock
